6d474add721644f69b9676e5df63d9a0eeaeee85
mkrenzke <krenzke@unipoin.com>
2024-08-28 21:20:42 +0200

    refactor: Rearrange server code by separating into multiple files

---
+++ server/middleware/authorize.js
@@ -0,0 +1,14 @@
+const jwt = require('njwt');
+
+module.exports = (req, res, next) => {
+
+  const token = req.headers.authorization.split('Bearer ')[1];
+
+  try {
+    jwt.verify(token, process.env.OPEN_BALENA_JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(401).json({ success: false, message: 'Invalid token' });
+    throw new Error('Unauthorized');
+  }
+}
---
+++ server/util/s3/client.js
@@ -0,0 +1,19 @@
+const Minio = require('minio');
+
+require('dotenv').config();
+
+const registryBucket = process.env.OPEN_BALENA_S3_REGISTRY_BUCKET || 'registry-data';
+
+const minioClient = new Minio.Client({
+  endPoint: process.env.OPEN_BALENA_S3_URL.split('://')[1],
+  useSSL: !!process.env.OPEN_BALENA_S3_URL.includes('https'),
+  accessKey: process.env.OPEN_BALENA_S3_ACCESS_KEY,
+  secretKey: process.env.OPEN_BALENA_S3_SECRET_KEY,
+});
+
+module.exports = {
+  s3Client: minioClient,
+  bucketNames: {
+    registry: registryBucket
+  }
+};
