f66a7f5ea0554cb839a6b47c1974001bc8e1efb9
mkrenzke <krenzke@unipoin.com>
2024-08-29 08:09:01 +0200

    feat: Throw error if OPEN_BALENA_S3_URL is not set in env

--- server/util/s3/client.js
+++ server/util/s3/client.js
@@ -1,7 +1,9 @@
 const Minio = require('minio');
 require('dotenv').config();
 
-const registryBucket = process.env.OPEN_BALENA_S3_REGISTRY_BUCKET || 'registry-data';
+if (!process.env.OPEN_BALENA_S3_URL || typeof process.env.OPEN_BALENA_S3_URL !== 'string') {
+  throw new Error('OPEN_BALENA_S3_URL is not set in environment');
+}
 
 module.exports = new Minio.Client({
   endPoint: process.env.OPEN_BALENA_S3_URL.split('://')[1],
